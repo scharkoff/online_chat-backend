e6e1241140bcb80a9d551e528b288facc0a69f24
Sharkov Ivan <sharkov.agent@mail.ru>
2023-02-13 12:53:06 +0300

    Customized Prettier
    - Set up prettier for typescript in the project

--- index.ts
+++ index.ts
@@ -1,27 +1,21 @@
-import express, { json } from "express";
-import http from "http";
-import { Server, Socket } from "socket.io";
+import express, { json } from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:4001",
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: 'http://localhost:4001', methods: ['GET', 'POST'] }
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-   console.log(req);
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log(req);
 });
 
-io.on("connection", (socket: Socket ) => {
-    console.log("user connected: ", socket.id);
-})
-
+io.on('connection', (socket: Socket) => {
+  console.log('user connected: ', socket.id);
+});
 
 server.listen(4000, () => {
-    console.log("Server started on port 4000...")
+  console.log('Server started on port 4000...');
 });
-
-
