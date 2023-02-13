60d61ff45315ba57b7da1feb2a7f9210a8856d22
Sharkov Ivan <sharkov.agent@mail.ru>
2023-02-13 12:11:48 +0300

    Setup cors headers
    - Choose origin cllient with methods in cors headers

--- index.ts
+++ index.ts
@@ -4,14 +4,19 @@ import { Server, Socket } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:4001",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
    console.log(req);
 });
 
 io.on("connection", (socket: Socket ) => {
-    console.log("user conndecte: ", socket);
+    console.log("user connected: ", socket.id);
 })
 
 
