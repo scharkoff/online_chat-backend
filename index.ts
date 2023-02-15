f08b82df723a834123aca2ed6b85d0106b3e6af7
Sharkov Ivan <sharkov.agent@mail.ru>
2023-02-15 15:59:51 +0300

    Online display system implemented
    - Get request for rooms by room id returns the current list of users in a particular room
    - Post request for rooms implements user accession to a specific room
    - When a user disconnects, the list changes online

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { json } from 'express';
+import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
 import cors from 'cors';
@@ -15,25 +15,67 @@ const io = new Server(server, {
 });
 
 app.use(cors());
+app.use(express.json());
+
+app.get(
+  '/rooms/:id',
+  (req: express.Request, res: express.Response) => {
+    const roomId = req.params.id || '';
+
+    const users = rooms.has(roomId)
+      ? rooms.get(roomId)?.get('users')?.values() || []
+      : [];
+
+    const messages =
+      rooms.get(roomId)?.get('messages')?.values() || [];
+
+    const obj = {
+      users: [...users],
+      messages: [...messages]
+    };
+
+    return res.json(obj);
+  }
+);
 
 app.post('/rooms', (req: express.Request, res: express.Response) => {
-  return res.send('Вернул комнаты');
+  const { roomId } = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(
+      roomId,
+      new Map([
+        ['users', new Map()],
+        ['messages', new Map()]
+      ])
+    );
+  }
+
+  return res.status(200).send('Joined successful');
 });
 
 io.on('connection', (socket: Socket) => {
   socket.on('ROOM:JOIN', ({ roomId, userName }: IDataProps) => {
     socket.join(roomId);
-    rooms.get('roomId')?.get('users')?.set(socket.id, userName);
+    rooms.get(roomId)?.get('users')?.set(socket.id, userName);
 
-    const users = rooms.get('roomId')?.get('users')?.values() || [];
+    const users = rooms.get(roomId)?.get('users')?.values() || [];
 
     socket.broadcast.to(roomId).emit('ROOM:JOINED', [...users]);
   });
-  console.log('user connected: ', socket.id);
-});
 
-io.on('disconnect', (socket: Socket) => {
-  console.log(socket.id, ' has disconnect');
+  socket.on('disconnect', () => {
+    rooms.forEach((value, roomId) => {
+      if (value.get('users')?.delete(socket.id)) {
+        const users = rooms.get(roomId)?.get('users')?.values() || [];
+
+        socket.broadcast
+          .to(roomId)
+          .emit('ROOM:UPDATE_USERS', [...users]);
+      }
+    });
+  });
+
+  console.log('user connected: ', socket.id);
 });
 
 server.listen(4000, () => {
--- src/data/rooms.ts
+++ src/data/rooms.ts
@@ -1,11 +1,3 @@
 import { TRoomsMap } from './types';
 
-export const rooms: TRoomsMap = new Map([
-  [
-    'roomId',
-    new Map([
-      ['users', new Map()],
-      ['messages', new Map()]
-    ])
-  ]
-]);
+export const rooms: TRoomsMap = new Map();
